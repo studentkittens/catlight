71cb6df384d41ba0d837c013c552ecf0d6541394
Chris P <sahib@online.de>
2012-12-07 23:50:43 +0100

    Kaminfeuer der Lust

--- new/effects.py
+++ new/effects.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 
 import color
+import random
 
 
 class SimpleFade(object):
@@ -30,3 +31,33 @@ class SimpleFade(object):
             g = self._col[1] * i * self._speed
             b = self._col[2] * i * self._speed
             yield color.Color(r, g, b)
+
+
+class KaminFeuerDerLust(object):
+    def __iter__(self):
+        def calc_color(t, num=1024, fac=1.0, jitter=20):
+            jitter_fac = random.randrange(-jitter, jitter)
+            kamin_func = lambda x: -255./262144 * ((x - num/2) ** 2) + 255
+            return fac * (kamin_func(t) + jitter_fac)
+
+        NUM = 1024
+        for t in range(NUM):
+            b = calc_color(t, num=NUM, fac=1.0, jitter=50)
+            g = calc_color(t, num=NUM, fac=0.15, jitter=100)
+            r = calc_color(t, num=NUM, fac=0.01, jitter=10)
+
+            yield color.Color(r, g, b, time=15)
+
+        # Glut der Lust.
+        yield color.Color(0, 2, 25, time=1000)
+
+
+class Repeater(object):
+    def __init__(self, effect, count=10):
+        self._effect = effect
+        self._count = count
+
+    def __iter__(self):
+        for i in range(self._count):
+            for col in self._effect:
+                yield col
